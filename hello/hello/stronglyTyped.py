f956024725e6b78e6698141bd7293faaf9bdd34f
ana_marquez <anamarquez.edu@hotmail.com>
2019-07-22 16:48:04 -0700

    DYNAMICALLY LANGUAGE

---
+++ hello/hello/stronglyTyped.py
@@ -0,0 +1,24 @@
+# DYNAMICALLY LANGUAGE fundamentals of python everything in python is an object
+
+a = 1
+# len(1)
+type(a)
+
+str = "value"
+str.upper()
+# a.uper() error
+
+type(1.5)  # float
+
+type(True)  # bool
+
+type(str)  # string
+
+str = 1
+type(str)
+
+str = True
+type(str)
+
+str = [1, 2]
+type(str)

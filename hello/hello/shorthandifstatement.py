6c77865123ce4302414f98d2ff766758602efe78
ana_marquez <anamarquez.edu@hotmail.com>
2019-07-22 16:36:01 -0700

    short if statement

---
+++ hello/hello/shorthandifstatement.py
@@ -0,0 +1,17 @@
+number = 5
+if(number % 2 == 0):
+    isEven = True
+else:
+    isEven = False
+
+print(isEven)
+
+
+Even = True if number % 2 == 0 else False
+print(Even)
+
+isEv = number % 2 == 0
+print(isEv)
+
+isE = "Yes" if number%2==0 else "No"
+print(isE)

3ad7f24a1eb0f6342d61785dde0a3c3f1a1023ba
ana_marquez <anamarquez.edu@hotmail.com>
2019-09-19 09:43:59 -0700

    arguments

--- Moretips/methods_arguments.py
+++ Moretips/methods_arguments.py
@@ -1,11 +1,10 @@
 def example_method(mandatory_parameter, default_parameter="Default", *args, **kwargs):
     print(f"""
-    mandatory_parameter = {mandatory_parameter}
-    default_parameter = {default_parameter}
-    args = {args}
-    kwargs = {kwargs}
+    mandatory_parameter = {mandatory_parameter} {type(mandatory_parameter)}
+    default_parameter = {default_parameter} {type(default_parameter)}
+    args = {args} {type(args)}
+    kwargs = {kwargs} {type(kwargs)}
     """)
-
 
    # example_method()
 example_method(15)
@@ -13,3 +12,11 @@ example_method(mandatory_parameter=10)
 example_method(25, 45)
 example_method(25, "Ana")
 example_method(25, "String 1", "String 2", "string 3")
+example_method(25, "String 1", "String 2", "string 3", key='a', key2='b')
+example_method(25, "String 1", key='a', key2='b')
+example_method(key='a', key2='b', mandatory_parameter=25, default_parameter="String 1")
+example_method(key='a', key2='b', mandatory_parameter=25,
+               default_parameter="String 1")
+
+example_list = [1,2,3]
+example_method(example_list[0], example_list[1], example_list[2])

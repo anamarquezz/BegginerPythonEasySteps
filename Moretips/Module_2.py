3268ab97e90149996b4b1d5ea1f5eeb051c2ea46
ana_marquez <anamarquez.edu@hotmail.com>
2019-09-19 14:59:55 -0700

    modules in python, import file in another

---
+++ Moretips/Module_2.py
@@ -0,0 +1,3 @@
+import module_1 #calling another python file
+module_1.method_1()
+module_1.ClassA().class_method_1()
---
+++ Moretips/module_1.py
@@ -0,0 +1,19 @@
+# CAN CONTAIN A MULTIPLE CLASSES AND METHODS
+# A MODULE, SET OF CLASES, METHODS, AND ALL HAVE IN PYTHON FILE
+
+
+def method_1():
+    print("method 1")
+
+
+class ClassA:
+    def class_method_1(self):
+        print("class method_1 method_1")\
+
+
+
+# print(__name__)
+
+if __name__ == '__main__':
+    method_1()
+    ClassA().class_method_1()

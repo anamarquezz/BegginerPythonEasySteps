894c69309a708c9ded8e58c6f117964548e86ca9
ana_marquez <anamarquez.edu@hotmail.com>
2019-09-19 16:35:31 -0700

    None

---
+++ Moretips/none.py
@@ -0,0 +1,23 @@
+print(None)
+print(type(None))
+
+
+def email(subject, content, to, cc=None, bcc=None):
+    print(f"{subject}, {content}, {to}, {cc}, {bcc}")
+
+
+def email2(subject, content, to, cc, bcc):
+    print(f"{subject}, {content}, {to}, {cc}, {bcc}")
+
+
+email("subject", "great work", "Correo@correo")
+email2("subject", "great work", "Correo@correo", None, None)
+
+var = "123"
+if var is None:
+    print("do something").
+
+
+var = None
+if var is None:
+    print("do something")
